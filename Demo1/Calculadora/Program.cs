c7319923133fde389a3a490de16d8e5f556d85f8
Moises Chirinos <moises.chirinos@leuchterag.ch>
2018-05-10 18:15:06 -0500

    Demo-1: Clase 05-08-2018

---
+++ Demo1/Calculadora/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 0;
+            var b = 0;
+            Console.WriteLine("Elija una opcion:");
+            Console.WriteLine("Sumar");
+            Console.WriteLine("Multiplicar");
+            Console.WriteLine("Dividir");
+            Console.WriteLine("Salir");
+            bool ejecutando = true;
+            while (ejecutando)
+            {
+                var opcion = Console.ReadLine();
+                opcion = opcion.ToLower();
+                switch (opcion)
+                {
+                    case "sumar":
+                        a = Convert.ToInt32(Console.ReadLine());
+                        b = Convert.ToInt32(Console.ReadLine());
+                        var suma = a + b;
+                        Console.WriteLine(suma);
+                        break;
+                    case "multiplicar":
+                        a = Convert.ToInt32(Console.ReadLine());
+                        b = Convert.ToInt32(Console.ReadLine());
+                        var multiplicacion = a * b;
+                        Console.WriteLine(multiplicacion);
+                        break;
+                    case "dividir":
+                        a = Convert.ToInt32(Console.ReadLine());
+                        b = Convert.ToInt32(Console.ReadLine());
+                        double division =(double) a / b;
+                        Console.WriteLine(division);
+                        break;
+                    case "salir":
+                        ejecutando = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            string entrada = Console.ReadLine();
+
+            Console.WriteLine("El usuario introdujo:" + entrada);
+            Console.ReadKey();
+            int numeroEntero = 123456;
+            long numeroLong = 1234556778;
+            float f = 12.123f;
+            double d = 12.3123123;
+            decimal de = 12.23213M;
+            string strings = "12345";
+            char c = 'c';
+            bool b = true | false;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Hola");
+            sb.AppendLine("Mundo");
+            Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
