2f6d74452bd8d4cd530cd67a192a248b5e1d8262
Moises Chirinos <moises.chirinos@leuchterag.ch>
2018-05-31 19:40:27 -0500

    Clase-7: Added Client services http and proxy

--- Clase-7-WebServices/WebClient/WebClient/Program.cs
+++ Clase-7-WebServices/WebClient/WebClient/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 namespace WebClient
 {
@@ -10,9 +12,38 @@ namespace WebClient
     {
         static void Main(string[] args)
         {
-            //http://localhost:56540/Service1.svc
 
-            //
+            //http://localhost:56527/api/values
+            // Web client para diferentes tipos de request
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+            // Solo request HTTP
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:56527/");
+
+            var task = httpClient.GetAsync("api/values");
+            task.Wait();
+
+            HttpResponseMessage result = task.Result;
+            var texto = result.Content.ReadAsStringAsync();
+            texto.Wait();
+            Console.WriteLine(texto.Result);
+
+            Console.WriteLine("-----Fin del consumo de HTTP ---");
+            Console.ReadKey();
+
+
+            //http://localhost:56540/Service1.svc
+            SR.Service1Client s1c = new SR.Service1Client();
+            Console.WriteLine(s1c.GetData(10));
+            var resultado = s1c.GetDataUsingDataContract(new SR.CompositeType()
+            {
+                BoolValue = true,
+                StringValue = "Moises Chirinos "
+            });
+            Console.WriteLine(resultado.StringValue);
+
+            Console.ReadKey();
         }
     }
 }
