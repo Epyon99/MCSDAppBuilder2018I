efa7e6dbde975891ae3bd4c4f026116424481da4
Moises Chirinos <moises.chirinos@leuchterag.ch>
2018-05-31 19:19:39 -0500

    Clase-7: Added demo Web Services to Practice

---
+++ Clase-7-WebServices/WebClient/WebClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://localhost:56540/Service1.svc
+
+            //
+        }
+    }
+}
